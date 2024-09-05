1d26a211210358e068c2dedb95f502efcf575186
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-05 16:06:53 +0200

    img

--- src/Controller/DropboxController.php
+++ src/Controller/DropboxController.php
@@ -15,24 +15,26 @@ class DropboxController extends AbstractController
     #[Route('/dropbox', name: 'dropbox')]
     public function index(Request $request)
     {
-        $accessToken = getenv('DROPBOX_ACCESS_TOKEN');
-        $dropboxApp = new DropboxApp("t03ew4kslhdea50", "lzizv35rwznpive", $accessToken);
-        $dropbox = new Dropbox($dropboxApp);
+        if (ENV == 'prod') {
+            $accessToken = getenv('DROPBOX_ACCESS_TOKEN');
+            $dropboxApp = new DropboxApp("t03ew4kslhdea50", "lzizv35rwznpive", $accessToken);
+            $dropbox = new Dropbox($dropboxApp);
 
-        $path = '/1.png';
-        $temporaryLink = $dropbox->getTemporaryLink($path);
+            $path = '/1.png';
+            $temporaryLink = $dropbox->getTemporaryLink($path);
 
-        if ($request->getMethod() === 'POST') {
-            $file = $request->files->get('image');
-            $dropboxFile = new DropboxFile($file->getPathname());
-            $uploadedFile = $dropbox->upload($dropboxFile, "/images/{$file->getClientOriginalName()}", ['mode' => 'overwrite']);
+            if ($request->getMethod() === 'POST') {
+                $file = $request->files->get('image');
+                $dropboxFile = new DropboxFile($file->getPathname());
+                $uploadedFile = $dropbox->upload($dropboxFile, "/images/{$file->getClientOriginalName()}", ['mode' => 'overwrite']);
 
-            return new Response("Fichier uploadé avec succès !");
+                return new Response("Fichier uploadé avec succès !");
+            }
         }
 
         return $this->render('dropbox/image.html.twig', [
             // 'temporaryLink' => $temporaryLink,
-            'temporaryLink' => $temporaryLink->link,
+            'temporaryLink' => $temporaryLink->link ?? null,
         ]);
     }
 }
