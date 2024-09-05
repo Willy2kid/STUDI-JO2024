601c2dee2001835030d57b212a3bdbc1fe40db28
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-05 15:56:11 +0200

    img

--- src/Controller/DropboxController.php
+++ src/Controller/DropboxController.php
@@ -25,7 +25,7 @@ class DropboxController extends AbstractController
         if ($request->getMethod() === 'POST') {
             $file = $request->files->get('image');
             $dropboxFile = new DropboxFile($file->getPathname());
-            $uploadedFile = $dropbox->upload($dropboxFile, "/images/{$file->getClientOriginalName()}", ['autorename' => false]);
+            $uploadedFile = $dropbox->upload($dropboxFile, "/images/{$file->getClientOriginalName()}", ['mode' => 'overwrite']);
 
             return new Response("Fichier uploadé avec succès !");
         }
