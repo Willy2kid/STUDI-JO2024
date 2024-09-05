603e4a8442b8e33b9337fcccf3fb9e26564b1c8b
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-05 14:12:28 +0200

    img

--- src/Controller/DropboxController.php
+++ src/Controller/DropboxController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DropboxController extends Controller
+class DropboxController extends AbstractController
 {
     #[Route('/dropbox', name: 'dropbox')]
     public function index()
