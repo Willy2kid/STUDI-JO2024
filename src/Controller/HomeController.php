89ad95c8fa93dfc97a1cf5952d142b9f6df9947a
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-06 05:49:21 +0200

    img

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,22 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\ImageHandler;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, ImageHandler $imageHandler): Response
     {
         $products = $productRepository->findAll();
+
+        if (getenv('APP_ENV') == 'prod') {
+            $productsImgLinks = $imageHandler->getImageLink('product', $products);
+        }
+
         return $this->render('home/index.html.twig', [
             'products' => $products,
+            'links' => $links ?? [],
         ]);
     }
 }
