68150d6aae4f7b93043dda5ca38305e98ca59304
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-07 05:31:59 +0200

    img

--- src/Service/ImageHandler.php
+++ src/Service/ImageHandler.php
@@ -37,6 +37,32 @@ class ImageHandler
         
     }
 
+    // public function getImageLink($imgDir, $items)
+    // {
+    //     $accessToken = getenv('DROPBOX_ACCESS_TOKEN');
+    //     $dropboxApp = new DropboxApp("t03ew4kslhdea50", "lzizv35rwznpive", $accessToken);
+    //     $dropbox = new Dropbox($dropboxApp);
+    // 
+    //     $links = [];
+    // 
+    //     foreach ($items as $item) {
+    //         $fileName = $item->getId() . '.png';
+    //         $filePath = '/images/' . $imgDir . '/' . $fileName;
+    //         $folderPath = '/images/' . $imgDir;
+    //         $searchResult = $dropbox->search($folderPath, $fileName);
+    //         $path = '/images/product/1.png';
+    //
+    //         if ($searchResult) {
+    //             $link = $dropbox->getTemporaryLink($path);
+    //             $links[$item->getId()] = $link->url;
+    //             // $links[] = $dropbox->getTemporaryLink($searchResult->getItems()->first()->getPath());
+    //             // echo 'Lien généré pour ' . $name . ' sur dropbox et doit être égale à ' . $fileName;
+    //         }
+    //     }
+    //     $this->logger->info('tableau des liens: ' . count($links));
+    //     return $links;
+    // }
+
     public function getImageLink($imgDir, $items)
     {
         $accessToken = getenv('DROPBOX_ACCESS_TOKEN');
@@ -44,26 +70,27 @@ class ImageHandler
         $dropbox = new Dropbox($dropboxApp);
     
         $links = [];
-        // $folderContents = $dropbox->listFolder('/images/' . $imgDir . '/');
-
-        // echo "CCCCCCCCCCCCCCCCCCCCCCC";
-        // exit; // or die;
+        $folderPath = '/images/' . $imgDir;
     
+        // Batch the API calls to reduce the number of calls made
+        $batch = [];
         foreach ($items as $item) {
             $fileName = $item->getId() . '.png';
-            $filePath = '/images/' . $imgDir . '/' . $fileName;
-            $folderPath = '/images/' . $imgDir;
-            $searchResult = $dropbox->search($folderPath, $fileName);
-            $path = '/images/product/1.png';
-
-            if ($searchResult) {
-                $link = $dropbox->getTemporaryLink($path);
-                $links[$item->getId()] = $link->url;
-                // $links[] = $dropbox->getTemporaryLink($searchResult->getItems()->first()->getPath());
-                // echo 'Lien généré pour ' . $name . ' sur dropbox et doit être égale à ' . $fileName;
-            }
+            $batch[] = $fileName;
         }
+    
+        // Make a single API call to search for all files in the batch
+        $searchResult = $dropbox->search($folderPath, implode(',', $batch));
+    
+        // Loop through the search results and get the temporary link for each file
+        foreach ($searchResult->getItems() as $file) {
+            $link = $dropbox->getTemporaryLink($file->getPath());
+            $links[$file->getName()] = $link->url;
+        }
+    
+        // Reduce logging to improve performance
         $this->logger->info('tableau des liens: ' . count($links));
+    
         return $links;
     }
 }
