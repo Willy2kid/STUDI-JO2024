11f1c2de237ff13a7211bb33af4f430307f45ede
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-07 12:36:27 +0200

    img

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\ImageHandler;
@@ -11,12 +12,13 @@ use App\Service\ImageHandler;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(ProductRepository $productRepository, ImageHandler $imageHandler): Response
+    public function index(Request $request, ProductRepository $productRepository, ImageHandler $imageHandler): Response
     {
         $products = $productRepository->findAll();
 
         if (getenv('APP_ENV') == 'prod') {
-            $productsImgLinks = $imageHandler->getImageLink('product', $products);
+            $dropboxAccessToken = $request->getSession()->get('dropbox_access_token');
+            $productsImgLinks = $imageHandler->getImageLink('product', $products, $dropboxAccessToken);
         }
 
         return $this->render('home/index.html.twig', [
