dfa2bb3518d1dfd965a4e42568ff6c0d5f037d34
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-07 14:18:23 +0200

    img

--- src/Controller/DropboxCallbackController.php
+++ src/Controller/DropboxCallbackController.php
@@ -2,24 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\DropboxService;
 
 class DropboxCallbackController extends AbstractController
 {
-    private $dropboxService;
-
-    public function __construct(DropboxService $dropboxService)
-    {
-        $this->dropboxService = $dropboxService;
-    }
-
     #[Route('/dropbox/callback', name: 'dropbox_callback')]
-    public function index(Request $request)
+    public function callback(Request $request, DropboxService $dropboxService)
     {
-        $this->dropboxService->callback();
-        return $this->dropboxService->callback($request);
+        $dropboxService->callback();
+        return $dropboxService->callback($request);
     }
 }
