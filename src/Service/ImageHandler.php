d50f374d6fbd346eb8791dcedb71622d2868f0ce
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-07 13:11:18 +0200

    img

--- src/Service/ImageHandler.php
+++ src/Service/ImageHandler.php
@@ -23,7 +23,7 @@ class ImageHandler
         // $this->appKey = $parameterBag->get('DROPBOX_APP_KEY');
         // $this->appSecret = $parameterBag->get('DROPBOX_APP_SECRET');
         $this->appSecret = $parameterBag->get('env(DROPBOX_APP_SECRET)');
-        $this->appKey = $parameterBag->get('env(DROPBOX_APP_KEY)');
+        $this->appKey = $parameterBag->get('env(DROPBOX_KEY)');
         dump($this->appKey, $this->appSecret);
     }
 
