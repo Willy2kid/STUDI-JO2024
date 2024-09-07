e84e963587c098685fb34fd725465f2b76b1d011
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-07 10:21:12 +0200

    img

--- src/Service/ImageHandler.php
+++ src/Service/ImageHandler.php
@@ -63,14 +63,15 @@ class ImageHandler
     private function getAccessToken(){
         $appKey = 't03ew4kslhdea50';
         $appSecret = 'lzizv35rwznpive';
-        $accessToken = 'pbaDUVvIPpkAAAAAAAAAGteGYoA5yZEWUuLg65Q9OXE';
+        $accessCode = $this->getAccessCode();
+        // $accessCode = 'pbaDUVvIPpkAAAAAAAAAGteGYoA5yZEWUuLg65Q9OXE';
 
         $client = new Client();
 
         $response = $client->post('https://api.dropboxapi.com/oauth2/token', [
             'auth' => [$appKey, $appSecret],
             'form_params' => [
-                'code' => $accessToken,
+                'code' => $accessCode,
                 'grant_type' => 'authorization_code',
             ],
             'headers' => [
@@ -86,6 +87,30 @@ class ImageHandler
             return $token;
         } else {
             echo "Erreur lors de la récupération du token d'accès";
+        }
+    }
+
+    private function getAccessCode(){
+        $client = new Client();
+
+        $clientId = 't03ew4kslhdea50';
+
+        $uri = 'https://www.dropbox.com/oauth2/authorize';
+        $queryParams = [
+            'client_id' => $clientId,
+            'token_access_type' => 'offline',
+            'response_type' => 'code',
+        ];
+
+        $response = $client->get($uri, ['query' => $queryParams]);
+
+        if ($response->getStatusCode() === 200) {
+            $location = $response->getHeader('Location');
+            $code = substr($location[0], strpos($location[0], '=') + 1);
+        
+            return $code;
+        } else {
+            echo "Erreur lors de la récupération du code d'accès";
         }
     }
 }
