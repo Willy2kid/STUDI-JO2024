ba95090f8fce9439eae7c76bb41ed46296226dce
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-07 14:38:28 +0200

    img

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,23 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Service\ImageHandler;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(Request $request, ProductRepository $productRepository, ImageHandler $imageHandler): Response
+    public function index(Request $request, ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
 
-        if (getenv('APP_ENV') == 'prod') {
-            $dropboxAccessToken = $request->getSession()->get('dropbox_access_token');
-            $productsImgLinks = $imageHandler->getImageLink('product', $products, $dropboxAccessToken);
-        }
-
         return $this->render('home/index.html.twig', [
             'products' => $products,
-            'productsImgLinks' => $productsImgLinks ?? [],
         ]);
     }
 }
--- src/Service/ImageHandler.php
+++ src/Service/ImageHandler.php
@@ -2,28 +2,14 @@
 
 namespace App\Service;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Kunnu\Dropbox\Dropbox;
-use Kunnu\Dropbox\DropboxApp;
-use Kunnu\Dropbox\DropboxFile;
-use GuzzleHttp\Client;
-use App\Service\DropboxService;
 
 class ImageHandler
 {
     private $kernel;
-    private $dropboxService;
-    private $appKey;
-    private $appSecret;
 
-    public function __construct(KernelInterface $kernel, DropboxService $dropboxService, ParameterBagInterface $parameterBag)
+    public function __construct(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
-        $this->dropboxService = $dropboxService;
-        // $this->appKey = $parameterBag->get('DROPBOX_APP_KEY');
-        // $this->appSecret = $parameterBag->get('DROPBOX_APP_SECRET');
-        // $this->appSecret = $parameterBag->get('env(DROPBOX_APP_SECRET)');
-        // $this->appKey = $parameterBag->get('env(DROPBOX_APP_KEY)');
     }
 
     public function uploadImage($image, $productId)
@@ -43,30 +29,5 @@ class ImageHandler
 
         $image->move($productImgDir, $imageName);
         
-    }
-
-    public function getImageLink($imgDir, $items, string $accessToken, ParameterBagInterface $parameterBag)
-    {
-        $appSecret = getenv('DROPBOX_APP_SECRET');
-        $appKey = getenv('DROPBOX_APP_KEY');
-        $dropboxApp = new DropboxApp($appKey, $appSecret, $accessToken);
-        $dropbox = new Dropbox($dropboxApp);
-
-        $links = [];
-        $itemIds = [];
-        foreach ($items as $item) { $itemIds[] = $item->getId(); }
-        $folderPath = '/images/' . $imgDir .'/';
-        $files = $dropbox->listFolder($folderPath)->getItems();
-
-        foreach ($files as $file) {
-            $fileName = $file->getName();
-            $fileNameNoExt = str_replace('.png', '', $fileName);
-            if (in_array($fileNameNoExt, $itemIds)) {
-                $link = $dropbox->getTemporaryLink($folderPath . $fileName)->getLink();
-                $links[$fileNameNoExt] = $link;
-            }
-        }
-
-        return $links;
     }
 }
