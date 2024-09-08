70e68eab5e99fc39f0a0b798d330081c002020ec
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-09-08 07:26:04 +0200

    mailer

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -17,8 +17,11 @@ class MailerService
 
     public function sendWelcomeEmail(User $user)
     {
+        $senderEmail = 'jo2024-studi@outlook.com';
+        $senderName = 'JO2024-STUDI Support';
+
         $email = (new Email())
-            ->from('jo2024-studi@outlook.com')
+            ->from([$senderEmail => $senderName])
             ->to($user->getEmail())
             ->subject('Bienvenue sur l\'application JO2024 - STUDI')
             ->html('Vous avez créé un compte sur l\'application JO2024 - STUDI. Votre identifiant est : ' . $user->getUsername());
