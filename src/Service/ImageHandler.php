45c146fe41d5d37b2ab1f81c205dc35d7151ccd2
Willy2kid <140309258+Willy2kid@users.noreply.github.com>
2024-10-04 12:08:30 +0200

    configuration de l'API dropbox

--- src/Service/ImageHandler.php
+++ src/Service/ImageHandler.php
@@ -2,6 +2,10 @@
 
 namespace App\Service;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Kunnu\Dropbox\Dropbox;
+use Kunnu\Dropbox\DropboxApp;
+use Kunnu\Dropbox\DropboxFile;
+use GuzzleHttp\Client;
 
 class ImageHandler
 {
@@ -29,5 +33,81 @@ class ImageHandler
 
         $image->move($productImgDir, $imageName);
         
+    }
+
+    public function getImageLink($imgDir, $items)
+    {
+        $appKey = getenv('DROPBOX_API_KEY');
+        $appSecret = getenv('DROPBOX_API_SECRET');
+        // $accessToken = getenv('DROPBOX_ACCESS_TOKEN');
+
+        $accessToken = $this->getAccessToken($appKey, $appSecret);
+        $dropboxApp = new DropboxApp($appKey, $appSecret, $accessToken);
+        $dropbox = new Dropbox($dropboxApp);
+
+        $links = [];
+        $itemIds = [];
+        foreach ($items as $item) { $itemIds[] = $item->getId(); }
+        $folderPath = '/images/' . $imgDir .'/';
+        $files = $dropbox->listFolder($folderPath)->getItems();
+
+        foreach ($files as $file) {
+            $fileName = $file->getName();
+            $fileNameNoExt = str_replace('.png', '', $fileName);
+            if (in_array($fileNameNoExt, $itemIds)) {
+                $link = $dropbox->getTemporaryLink($folderPath . $fileName)->getLink();
+                $links[$fileNameNoExt] = $link;
+            }
+        }
+
+        return $links;
+    }
+
+    private function getAccessToken($appKey, $appSecret){
+
+        $accessCode = $this->getAccessCode($appKey);
+        $client = new Client();
+
+        $response = $client->post('https://api.dropboxapi.com/oauth2/token', [
+            'auth' => [$appKey, $appSecret],
+            'form_params' => [
+                'code' => $accessCode,
+                'grant_type' => 'authorization_code',
+            ],
+            'headers' => [
+                'Content-Type' => 'application/x-www-form-urlencoded',
+            ],
+        ]);
+
+        if ($response->getStatusCode() === 200) {
+            $responseData = json_decode($response->getBody()->getContents(), true);
+            $token = $responseData['access_token'];
+        
+            return $token;
+        } else {
+            echo "Erreur lors de la récupération du token d'accès";
+        }
+    }
+
+    private function getAccessCode($clientId){
+        $client = new Client();
+
+        $uri = 'https://www.dropbox.com/oauth2/authorize';
+        $queryParams = [
+            'client_id' => $clientId,
+            'token_access_type' => 'offline',
+            'response_type' => 'code',
+        ];
+
+        $response = $client->get($uri, ['query' => $queryParams]);
+
+        if ($response->getStatusCode() === 200) {
+            $location = $response->getHeader('Location');
+            $code = substr($location[0], strpos($location[0], '=') + 1);
+        
+            return $code;
+        } else {
+            echo "Erreur lors de la récupération du code d'accès";
+        }
     }
 }
